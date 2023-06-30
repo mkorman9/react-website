851e2b6b1732b049ef27db89ec25c6c1dcc551af
Michal Korman <michal.korman@icloud.com>
2023-06-30 18:39:44 +0200

    add icon to button

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Button from "./Button/Button";
 const App = () => {
     return (
         <div className="container">
-            <Button text="Click me!" onClick={() => console.log('Clicked!')} />
+            <Button intent='add' text="Click me!" onClick={() => console.log('Clicked!')} />
         </div>
     );
 };
--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,7 +1,11 @@
 import './Button.css';
+import AddIcon from '../icons/add.svg';
+
+export type ButtonIntent = 'none' | 'add';
 
 export interface ButtonProps {
     text: string;
+    intent?: ButtonIntent;
     onClick?: () => void;
 }
 
@@ -11,9 +15,20 @@ const Button = (props: ButtonProps) => {
         props.onClick && props.onClick();
     };
 
+    const icon = (() => {
+        if (props.intent === 'add') {
+            return AddIcon;
+        }
+    })();
+
     return (
         <button type='button' className='button' onClick={onClick}>
-            { props.text }
+            <div className='button-content'>
+                {(props.intent && props.intent != 'none') &&
+                    <img className='button-icon' src={icon} alt='' />
+                }
+                <span>{ props.text }</span>
+            </div>
         </button>
     );
 };

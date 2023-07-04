db25d6cc1de5c7eae5723dfd512be2f641610307
Michal Korman <michal.korman@icloud.com>
2023-07-04 17:23:03 +0200

    extract firebase config to .env file

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBGRQhxXTFc-z_UB81xGziP6YUajLhqT3E',
-  authDomain: 'my-playground-project-391323.firebaseapp.com',
-  projectId: 'my-playground-project-391323',
-  storageBucket: 'my-playground-project-391323.appspot.com',
-  messagingSenderId: '71830756335',
-  appId: '1:71830756335:web:48be7de96cd98f4c8ff41a',
-  measurementId: 'G-CJ08F2GTJV'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 export const app = initializeApp(firebaseConfig);
